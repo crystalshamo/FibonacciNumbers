1345e6324ad40360e88787483b2b535dfcc07d3d
Crystal Shamo <crystal.shamo@gmail.com>
2022-10-12 14:28:39 -0400

    first commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+    public static void main(String[] args) throws Exception {
+        int n;
+        if (args.length > 0) {
+            try {
+                n = Integer.parseInt(args[0]);
+                System.out.println(fib(n));
+            } catch (NumberFormatException e) {
+                System.err.println("Argument " + args[0] + " must be an integer!");
+                System.exit(1);
+            }
+        }
+    }
+
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
